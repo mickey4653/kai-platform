e29c394fcac0b7f2896a0cce166a64068a7dcc5b
Aozhuo Zhang <zhangaozhuo6@gmail.com>
2024-12-09 15:41:57 -0500

    FinalStep page

---
+++ frontend/pages/finalsteps/index.js
@@ -0,0 +1,8 @@
+import MainAppLayout from '@/layouts/MainAppLayout';
+import FinalSteps from '@/templates/OnBoarding/FinalSteps/FinalSteps';
+
+const FinalStepsPage = () => {
+  return <FinalSteps />;
+};
+
+export default FinalStepsPage;
